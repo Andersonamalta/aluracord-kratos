ac5777e42219de2e02581cf8f2ec2549e5372491
Anderson <anderson_malta16@hotmail.com>
2022-01-25 22:43:03 -0300

    Segundo commit

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,8 @@
 import appConfig from '../config.json';
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
+import { useRouter} from 'next/router';
 
-function GlobalStyle() {
-    return (
-
-        <style global jsx>{`
-          * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            list-style: none;
-          }
-
-          body {
-            font-family: 'Open Sans', sans-serif;
-          }
-
-          /* App fit Height */ 
-            html, body, #__next {
-                min-height: 100vh;
-                display: flex;
-                flex: 1;
-            }
-
-            #__next {
-                flex: 1;
-            }
-
-            #__next > * {
-                flex: 1;
-            }
-          /* ./App fit Height */ 
-        `}</style>
-    );
-}
 
 function Title(props) {
     console.log(props)
@@ -71,10 +40,13 @@ function Title(props) {
 //export default HomePage
 
 export default function PaginaInicial() {
-    const username = 'peas';
+    //const username = 'Andersonamalta';
+    const [username, setUsername] = React.useState('');
+    const roteamento = useRouter();
+
+
     return (
         <>
-            <GlobalStyle />
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -102,6 +74,13 @@ export default function PaginaInicial() {
                     {/* Formulário */}
                     <Box
                         as="form"
+                        onSubmit = {function (event){
+                            event.preventDefault();
+                            console.log('Alguém submeteu um form');
+                            roteamento.push('/chat');
+                            //window.location.href = '/chat';
+
+                        }}
                         styleSheet={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                             width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -112,6 +91,15 @@ export default function PaginaInicial() {
                             {appConfig.name}
                         </Text>
                         <TextField
+                            value={username}
+                            onChange={function (event) {
+                                console.log('usuario digitou', event.target.value);
+                                // Onde está o valor?
+                                const valor = event.target.value;
+                                // Trocar o valor da variavel
+                                // Através do React e avise quem precisa
+                                setUsername(valor);
+                            }}
                             fullWidth
                             textFieldColors={{
                                 neutral: {
@@ -154,7 +142,13 @@ export default function PaginaInicial() {
                                 border: '1px solid',
                                 borderColor: appConfig.theme.colors.neutrals['700'],
                             }}
-                            src={`https://github.com/${username}.png`}
+
+                            src = {`${username.length <=2 
+                                ? 'https://i.ibb.co/Ch4m85T/erro.png'
+                                : `https://github.com/${username}.png`
+                            }`}
+                            
+                            //src={`https://github.com/${username}.png`}
                         />
                         <Text
                             variant="body4"
@@ -165,7 +159,7 @@ export default function PaginaInicial() {
                                 borderRadius: '1000px'
                             }}
                         >
-                            {username}
+                            {username.length <=2 ? "Usuário não encontrado " : `${username}` + (location ? ' - ' + location : '')}
                         </Text>
                     </Box>
                     {/* Photo Area */}
